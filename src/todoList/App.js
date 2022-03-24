0aaefa52dc45f9ceb5ba082f8539f39401ac6afd
Vidhya <53406303+vid083@users.noreply.github.com>
2022-03-24 13:40:34 +0530

    todoList from Udemy course

---
+++ src/todoList/App.js
@@ -0,0 +1,15 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn react'/>
+      <Todo text='Learn angular'/>
+      <Todo text='Learn java'/>
+     
+    </div>
+  );
+}
+
+export default App;
